cbcc4226b3c869c63c8c303ef44c4fd5815b0de4
Anshul Arora <a.99.anshul@gmail.com>
2020-09-03 07:34:09 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,13 +1,19 @@
 def turn_count(board)
   counter = 0
+<<<<<<< HEAD
   board.each do |n|
     if n == "X"
       counter += 1
     elsif n == "O"
+=======
+  for n in board do
+    while n == "X" || "O"
+>>>>>>> c79c84b3d90df6c2b8dbf473552c6f25bedcc0d1
       counter += 1
     end
   end
   counter
+<<<<<<< HEAD
 end
 
 def current_player(board)
@@ -16,4 +22,6 @@ def current_player(board)
   else
     "O"
   end
+=======
+>>>>>>> c79c84b3d90df6c2b8dbf473552c6f25bedcc0d1
 end
